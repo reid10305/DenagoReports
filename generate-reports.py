463340681a994d57d28f1e24fec320cf98553fb3
Daniel Pifer <daniel.pifer13@gmail.com>
2023-05-22 00:04:11 -0500

    cmd args

--- generate-reports.py
+++ generate-reports.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import sys
+import sys, getopt
 
-def generateUPSReport():
+def generateUPSReport(show, file):
     '''generate UPS report'''
 
-    data = pd.read_csv('UPS-Shipments_L30D.csv')
+    data = pd.read_csv(file)
     df = pd.DataFrame(data)
 
     # make qty positive
@@ -15,7 +15,7 @@ def generateUPSReport():
     df['totWeight'] = df['QTY'] * df['UNIT_WEIGHT']
     df['totVolume'] = df['QTY'] * df['UNIT_VOLUME'] / (12 ** 3)
     
-    generateUPSPlots(df)
+    generateUPSPlots(df, show)
     generateUPSGeneralReport(df)
 
 def generateUPSGeneralReport(df):
@@ -44,7 +44,7 @@ def generateUPSGeneralReport(df):
     file.writelines([line1, line2, line3, line4, line5])
     file.close()
 
-def generateUPSPlots(df):
+def generateUPSPlots(df, show):
     qtyByDay = df.plot(x='DATE', y = ['QTY'], kind='scatter', title = 'QTY Per Day', grid = True, rot=45,figsize=(20,10))
     qtyBySku = df.plot(x='SKU', y = 'QTY', kind='bar', title = 'QTY by SKU', rot=90, figsize=(20,10))
     volByDay = df.plot(x='DATE', y = ['totWeight', 'totVolume'], kind='line', title = 'Volume Per Day', grid = True, subplots=True, rot=45,figsize=(20,10))
@@ -57,11 +57,41 @@ def generateUPSPlots(df):
     qtyBySku.figure.savefig('QTY-By-SKU.pdf')
     plt.savefig('Volume-By-Day.pdf')
 
-    #plt.show()
+    if show:
+        plt.show()
 
 def generateLTLReport():
     '''generate LTL report'''
 
 if __name__ == '__main__':
+
+    showCharts = False
+    inputFile = 'UPS-Shipments_L30D.csv'
+    upsonly= False
+    ltlonly = False
+
+    # define arguments
+    opts, args = getopt.getopt(sys.argv[1:], shortopts='hs:u:l:', longopts=['input=', 'show=', 'upsonly=', 'ltlonly='])
+
+    # parse arguments
+    for opt, arg in opts:
+        if opt == '-h':
+            print('generate-reports.py -s <t/f> -u <t/f> -l <t/f> --input <inputfile>')
+
+        elif opt in ('-s', '--show'):
+            if arg == 't':
+                showCharts = True
+
+        elif opt in ('-u', '--upsonly'):
+            if arg == 't':
+                upsonly = True
+        
+        elif opt in ('-l', '--ltlonly'):
+            if arg == 't':
+                ltlonly = True
+
+        elif opt == '--input':
+            inputFile = arg
+
     generateUPSReport()
     generateLTLReport()
