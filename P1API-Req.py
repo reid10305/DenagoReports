d74eff24b0d97236359c3d658a4a7116fccbe6d8
Daniel Pifer <daniel.pifer13@gmail.com>
2023-05-24 22:12:48 -0500

    completed LTL tracking capability

--- P1API-Req.py
+++ P1API-Req.py
@@ -1,18 +1,41 @@
 import requests
 import json
 
-url = "https://api.priority1.com/v2/ltl/shipments/status"
+class P1Helper():
+  url = "https://api.priority1.com/v2/ltl/shipments/status"
 
-payload = json.dumps({
-  "identifierType": "PURCHASE_ORDER",
-  "identifierValue": "DEN1717"
-})
-headers = {
-  'X-API-KEY': 'b80ebbe6-d4d7-48ce-ba19-3686d455eac4',
-  'accept': 'application/json',
-  'Content-Type': 'application/json'
-}
+  def getTracking(self, shipmentID:str) -> str:
 
-response = requests.request("POST", url, headers=headers, data=payload)
+    payload = json.dumps({
+    "identifierType": "PURCHASE_ORDER",
+    "identifierValue": f"{shipmentID}"
+    })
+    headers = {
+      'X-API-KEY': 'b80ebbe6-d4d7-48ce-ba19-3686d455eac4',
+      'accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
 
-print(response.text)
+    response = requests.request("POST", self.url, headers=headers, data=payload)
+
+    print(response.text)
+    return response.text
+
+    
+  def track(self, shipmentID:str):
+    trackingResultJSON = json.loads(self.getTracking(shipmentID))
+    statuses = trackingResultJSON['shipments'][0]['trackingStatuses']
+
+    status = 'In Transit'
+    deliveryDate = ''
+
+    for i in statuses:
+      if (i['status'] == 'Completed') or ('Completed' in i['statusReason']) :
+        status = 'Delivered'
+        deliveryDate = i['timeStamp'][:10]
+
+    return {'status' : status,
+            'deliveryDate' : deliveryDate}
+
+
+    
